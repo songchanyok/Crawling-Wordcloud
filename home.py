bf821d1387852defefc95f22bce36c9219fd04f1
Chanhyeok Song <kacu0417@naver.com>
2023-11-23 03:03:26 +0900

    read_json_4

--- home.py
+++ home.py
@@ -14,10 +14,12 @@ import streamlit as st
 def run_home():
     client_id = "j1EkU_Uw0eHCSI7xIo6x"
     client_secret = "be0nqCUsny"
+    st.title("대시보드")
     st.markdown("## 네이버 뉴스 검색 키워드 \n"
                 "네이버 뉴스 검색을 통해 top keywords 들을 wordcloud 형태로 보여주는 대시보드입니다.")
     keyword = st.text_input("네이버 뉴스 검색")
-    if keyword != None:
+    if keyword:
+        st.write("You entered: ",keyword)
         encText = urllib.parse.quote(str(keyword))
         url = "https://openapi.naver.com/v1/search/news?query="+encText
 
@@ -33,4 +35,4 @@ def run_home():
             response_json = json.loads(response_body)
             st.dataframe(response_json)
         else:
-            st.markdown("Error Code:" + rescode)
+            st.markdown("Error Code")
