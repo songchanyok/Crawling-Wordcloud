9d1e6ffd7db9adb4f2f4aef0facad5f521dfbd97
Chanhyeok Song <kacu0417@naver.com>
2023-11-23 02:33:13 +0900

    read_json_3

--- app.py
+++ app.py
@@ -14,6 +14,23 @@ from streamlit_option_menu import option_menu
 client_id = "j1EkU_Uw0eHCSI7xIo6x"
 client_secret = "be0nqCUsny"
 
+def read_json(keyword):
+    encText = urllib.parse.quote(str(keyword))
+    url = "https://openapi.naver.com/v1/search/news?query="+encText
+
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id",client_id)
+    request.add_header("X-Naver-Client-Secret",client_secret)
+    response = urllib.request.urlopen(request)
+
+    rescode = response.getcode()
+    if(rescode==200):
+        response_body = response.read()
+        #print(response_body.decode('utf-8'))
+        response_json = json.loads(response_body)
+        st.dataframe(response_json)
+    else:
+        st.markdown("Error Code:" + rescode)
 
 def main():
     with st.sidebar:
@@ -23,22 +40,9 @@ def main():
         st.markdown("## 네이버 뉴스 검색 키워드 \n"
                 "네이버 뉴스 검색을 통해 top keywords 들을 wordcloud 형태로 보여주는 대시보드입니다.")
         keyword = st.text_input("네이버 뉴스 검색")
-        encText = urllib.parse.quote(str(keyword))
-        url = "https://openapi.naver.com/v1/search/news?query="+encText
-
-        request = urllib.request.Request(url)
-        request.add_header("X-Naver-Client-Id",client_id)
-        request.add_header("X-Naver-Client-Secret",client_secret)
-        response = urllib.request.urlopen(request)
-
-        rescode = response.getcode()
-        if(rescode==200):
-            response_body = response.read()
-            #print(response_body.decode('utf-8'))
-            response_json = json.loads(response_body)
-            st.dataframe(response_json)
-        else:
-            st.markdown("Error Code:" + rescode)
+        if keyword != None:
+            read_json(keyword=keyword)
+        
 
     else:
         print("error")
