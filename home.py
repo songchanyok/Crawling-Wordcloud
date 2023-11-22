f76885523d66f694f17600a92a60b96512adfe54
Chanhyeok Song <kacu0417@naver.com>
2023-11-23 03:08:35 +0900

    read_json_6

--- home.py
+++ home.py
@@ -17,22 +17,22 @@ def run_home():
     st.title("대시보드")
     st.markdown("## 네이버 뉴스 검색 키워드 \n"
                 "네이버 뉴스 검색을 통해 top keywords 들을 wordcloud 형태로 보여주는 대시보드입니다.")
-    keyword = st.text_input("네이버 뉴스 검색")
-    if keyword:
-        st.write("You entered: ",keyword)
-        encText = urllib.parse.quote(str(keyword))
-        url = "https://openapi.naver.com/v1/search/news?query="+encText
+    # keyword = st.text_input("네이버 뉴스 검색")
+    # if keyword:
+    #     st.write("You entered: ",keyword)
+    #     encText = urllib.parse.quote(str(keyword))
+    #     url = "https://openapi.naver.com/v1/search/news?query="+encText
 
-        request = urllib.request.Request(url)
-        request.add_header("X-Naver-Client-Id",client_id)
-        request.add_header("X-Naver-Client-Secret",client_secret)
-        response = urllib.request.urlopen(request)
+    #     request = urllib.request.Request(url)
+    #     request.add_header("X-Naver-Client-Id",client_id)
+    #     request.add_header("X-Naver-Client-Secret",client_secret)
+    #     response = urllib.request.urlopen(request)
 
-        rescode = response.getcode()
-        if(rescode==200):
-            response_body = response.read()
-            #print(response_body.decode('utf-8'))
-            response_json = json.loads(response_body)
-            st.dataframe(response_json)
-        else:
-            st.markdown("Error Code")
+    #     rescode = response.getcode()
+    #     if(rescode==200):
+    #         response_body = response.read()
+    #         #print(response_body.decode('utf-8'))
+    #         response_json = json.loads(response_body)
+    #         st.dataframe(response_json)
+    #     else:
+    #         st.markdown("Error Code")
