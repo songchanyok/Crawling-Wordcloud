cb184ee114ff82d740ee57fafdb1259b4afb9de7
Chanhyeok Song <kacu0417@naver.com>
2023-11-26 11:03:38 +0900

    update_29

--- home.py
+++ home.py
@@ -212,7 +212,7 @@ def run_home():
         
         st.pyplot(text_3)
 
-        st.markdown(f'새로 나온단어 {[i for i in date_1_keywords_dict.keys() if i not in date_2_keywords.keys() and date_3_keywords.keys()]}')
+        st.markdown(f'새로 나온단어 {[i for i in date_1_keywords_dict.keys() if i not in [date_2_keywords.keys(), date_3_keywords.keys()]]}')
 
       
 
